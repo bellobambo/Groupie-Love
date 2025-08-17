98778eaba4c64dfa12122629a6abe30dee6c9955
.env.local <87149450+bellobambo@users.noreply.github.com>
2025-08-17 22:51:40 +0100

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -46,5 +46,9 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+       baseBuilder: {
+      allowedAddresses: [
+        "0xbB516047b62B17cF7bf4F87031a67C995dF821aC"]
+    }
   });
 }
